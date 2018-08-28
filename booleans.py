ea7724a2553fea4e4a7ee0fc5429d6ae61e23913
Chris Ford <Chrisford51@gmail.com>
2018-08-28 00:43:10 +0000

    Booleans

---
+++ booleans.py
@@ -0,0 +1,19 @@
+##
+# Booleans are operators that allow you to covey True or False statements
+
+True 
+False
+# Needs to be capitalized.
+
+1 > 2 
+# Returns False
+
+1 == 1 
+# Returns True
+
+# Placeholder for undefined variable.
+# None key
+
+b = None 
+# This would allow us to define the b var later to prevent any errors 
+
