9c560545f177f8368cef73dc375c0cbd27b4388f
Chris Ford <Chrisford51@gmail.com>
2018-08-28 19:26:04 +0000

    Added Data Types

---
+++ dataTypes.py
@@ -0,0 +1,27 @@
+# Integers	
+int		
+# Whole numbers such as:  3 300 200
+
+# Floating Point 	
+float 	
+# Numbers with a decimal point: 2.3 4.6 100.0
+
+#Strings
+str 
+# Ordered sequence of characters: "hello" "Sammy" "200"
+
+# Lists
+list 
+# Ordered sequence of objects: [10, "hello", 200.3]
+
+# Dictionaries
+dict 
+# Unordered Key:Value pairs: {"mykey":"value", "name":"Chris"}
+
+# Tuples
+tup 
+# Ordered immutable sequence of objects: {"a","b"}
+
+# Booleans
+bool 
+# Logical value indicating True or False
