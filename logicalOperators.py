4611af469a047e97c6306e4683f25c47207da30f
Chris Ford <Chrisford51@gmail.com>
2018-08-29 02:02:23 +0000

    Logical Operators

---
+++ logicalOperators.py
@@ -0,0 +1,15 @@
+##
+# Chaining Comparison Operators
+# We can use logical operators to combine comparisons:
+and
+or
+not
+
+and needs both args to be True
+or needs either args to be True
+
+1 == 1 # Returns True
+# We use the not operator if we want to return the opposite bool. 
+
+not(1==1) # Returns false 
+not 1 == 1 # also works
